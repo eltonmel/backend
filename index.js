7b8070902134501d9795c5f4d16afdaf3f996a2b
Elton Mel <eltondemel2010@hotmail.com>
2020-04-27 21:26:17 -0300

    Autoload scripts, middlewares, database and routes implemented

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const consign = require('consign');
 
 const app = express();
+const db = require('./src/Config/Database');
+
+app.db = db;
+
+consign()
+  .then('./src/Config/Middlewares.js')
+  .then('./src/Config/Routes.js')
+  .into(app);
 
 app.listen(5000, () => {
   console.log('Backend executando...');
